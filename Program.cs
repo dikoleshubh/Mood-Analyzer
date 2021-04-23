1293259dedc78fe2c5c03df9285322fc87a7269d
dikoleshubh <dikole.shubh@gmail.com>
2021-04-24 01:20:03 +0530

    Mood Analyzer

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodTEst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mood analyzer");
+        }
+    }
+}
