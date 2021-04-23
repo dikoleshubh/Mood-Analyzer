dd80fb456fb848eccb0172365b2ca298e3e90bb1
dikoleshubh <dikole.shubh@gmail.com>
2021-04-24 01:39:41 +0530

    Mood Analyzer- SAD

---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Happy_Mood()
+        {  //   //Arrange;
+            MoodAnalyser mood = new MoodAnalyser("I am in happy mood");
+            string expected = "happy";
+
+            ////Act
+            string actual = mood.Analyser();
+
+            //Asert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
